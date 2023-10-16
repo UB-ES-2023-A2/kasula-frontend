b785ccdf4620b8a0ac64c0d6f549e910ee2a9c29
U01A6909 <ivan.mansilla-flores.external@capgemini.com>
2023-10-16 12:40:23 +0200

    feat: Logout screen in progress and add files

---
+++ frontend/src/components/Logout.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../css/Logout.css";
+import logo from "../logo.svg";
+
+function Logout() {
+  return (
+    <div className="logout-container">
+      <div className="header">
+        <img src={logo} alt="Kasula" className="logo" />
+        <h1>Kasula</h1>
+      </div>
+      <div className="logout-box">
+        <div className="logout-message">
+          <p>¿Desea cerrar sesión?</p>
+        </div>
+        <div className="button-container">
+          <button className="logout-button">Aceptar</button>
+          <button className="cancel-button">Cancelar</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Logout;
