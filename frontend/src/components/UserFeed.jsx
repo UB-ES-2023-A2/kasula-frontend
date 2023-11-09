d59b70971947e056383c21b7d41296651ea8677b
Ivan Mansilla Flores <72189801+ivanmansilla@users.noreply.github.com>
2023-11-09 13:00:14 +0100

    feat: show alert when there arent recipes

--- frontend/src/components/UserFeed.jsx
+++ frontend/src/components/UserFeed.jsx
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useAuth } from "./AuthContext";
 import "../css/UserFeed.css";
 import "../css/Transitions.css";
 import { CSSTransition } from "react-transition-group";
 import logo from "../assets/logo.png";
 import gyozas from "../assets/gyozas.jpg";
-import { Link, useHistory, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Container, Row, Col, Image } from "react-bootstrap";
 
 function UserFeed() {
   const [recipes, setRecipes] = useState([]);
   const navigate = useNavigate();
-  const [showLogout, setLogout] = useState(false);
-  const { token, logout } = useAuth();
   const [isLogged, setIsLogged] = useState(localStorage.getItem("logged") === "true");
 
 
@@ -21,20 +18,10 @@ function UserFeed() {
     fetch(process.env.REACT_APP_API_URL + "/recipe/")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setRecipes(data);
       })
       .catch((error) => console.error("Error al obtener recetas:", error));
   }, []);
-
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
-  };
-
-  const handleHide = () => {
-    setLogout(false);
-  };
 
   return (
     <Container fluid className="bg-image min-vh-100">
@@ -42,8 +29,8 @@ function UserFeed() {
           <Col sm={1} className="py-2"> 
             <Image src={logo} alt="KASULÀ" fluid />
             </Col>
-            <Col sm={8}></Col>
-            <Col sm={3}>
+            <Col sm={9}></Col>
+            <Col sm={2}>
               <div id='buttons-group' className="mt-4">
                 {!isLogged && ( 
                   <button className='btn btn-light' onClick={() => navigate("/login")}>
@@ -51,7 +38,6 @@ function UserFeed() {
                   </button>
                 )}
                 {isLogged && (<>
-                {console.log(">>>>>>LOGEADO", localStorage.getItem("logged"))}
                  <button className='btn btn-light me-2' onClick={() => navigate("/postRecipe")}>
                   Post Recipe
                 </button>
@@ -69,24 +55,30 @@ function UserFeed() {
           <Col sm={10}>
           <CSSTransition in={true} timeout={500} classNames="slideUp" appear>
           <div className="recipes-container mt-5">
-            {recipes.map((recipe) => (
-              <Link key={recipe._id} to={`/RecipeDetail/${recipe._id}`} className="recipe-link">
-                <div className="mt-4 p-3 shadow rounded d-flex align-items-center overflow-hidden" id='recipes-list'>
-                  <Row className="align-items-center">
-                    <Col sm={9}>
-                      <p className="font-large-bold fs-5 text-dark">{recipe.name}</p>
-                    </Col>
-                    <Col sm={3}>
-                      <Image
-                        src={recipe.image ? recipe.image : gyozas}
-                        alt={recipe.name}
-                        fluid
-                      />
-                    </Col>
-                  </Row>
-                </div>
-              </Link>
-            ))}
+            {recipes && recipes.length > 0 ? (
+              recipes.map((recipe) => (
+                <Link key={recipe._id} to={`/RecipeDetail/${recipe._id}`} className="recipe-link">
+                  <div className="mt-4 p-3 shadow rounded d-flex align-items-center overflow-hidden" id='recipes-list'>
+                    <Row className="align-items-center">
+                      <Col sm={9}>
+                        <p className="font-large-bold fs-5 text-dark">{recipe.name}</p>
+                      </Col>
+                      <Col sm={3}>
+                        <Image
+                          src={recipe.image ? recipe.image : gyozas}
+                          alt={recipe.name}
+                          fluid
+                        />
+                      </Col>
+                    </Row>
+                  </div>
+                </Link>
+              ))
+              ) : (
+              <div className="alert alert-warning" role="alert">
+                No hay recetas disponibles
+              </div>
+            )}
           </div>
           </CSSTransition> 
           </Col>
