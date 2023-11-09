57e49f23b888b2c58ab03e61dbc36d529cebd0bd
Antonio Tigri <entertainmentatv@gmail.com>
2023-11-09 21:38:11 +0100

    Slight PR Adjustments
    (reviewing is slow)
    
    - Moved all css to common or bootstrap classes
    - Fixed spacing/sizing/positioning in the card
    - Very slight refactors
    - Fixed Step display text

--- frontend/src/components/RecipeDetail.jsx
+++ frontend/src/components/RecipeDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../css/RecipeDetail.css";
+import "../css/common.css";
 import "../css/Transitions.css";
 import logo from "../assets/logo.png";
 import { useParams } from "react-router-dom";
@@ -7,6 +7,7 @@ import { CSSTransition } from "react-transition-group";
 import gyozas from '../assets/gyozas.jpg';
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import { Container, Row, Col, Image } from "react-bootstrap";
+import { StarFill } from "react-bootstrap-icons";
 import ImageModal from "./ImageModal";
 
 
@@ -39,7 +40,7 @@ function RecipeDetail() {
   
 
   return (
-      <Container fluid className="bg-image min-vh-100">
+      <Container fluid className="min-vh-100">
         <ImageModal
           show={showModal}
           onHide={handleCloseModal}
@@ -57,63 +58,58 @@ function RecipeDetail() {
             <Col sm={2}></Col>
             <Col sm={8}>
             <CSSTransition in={true} timeout={500} classNames="slideUp" appear>
-              <Container id='recipe-container' className="mt-5 text-center rounded box-shadow" style={{ backgroundColor: '#ffb79fe0'}}>
+              <Container className="mt-5 text-center box-rounded shadow" style={{ backgroundColor: '#ffb79fe0'}}>
                 <Row>
-                  <Col xs={12} md={6} lg={6}>
-                    <Col xs={10}>
+                  <Col xs={12} md={6} lg={6} className="p-4">
+                    <Col xs={12}>
                       <Image
                         src={recipe.image ?? gyozas}
                         alt={recipe.name}
-                        className="img-fluid mx-3 my-4 box-shadow"
+                        className="img-fluid shadow mb-3"
                         onClick={handleOpenModal}
                         style={{ cursor: "pointer" }}
                         fluid
                       />
-                      </Col>
-                    <Col xs={12}>
                       <h2 style={{ marginBottom: '1rem' }}>{recipe.name}</h2>
                     </Col>
-                    <Col md={10}>
-                      <div id='info-box' className="mt-5 pt-2 bg-light box-shadow">
+                    <Col md={12}>
+                      <div className="mt-5 pt-2 bg-light box-shadow">
                         <h4>Más información</h4>
-                        <div id='difficultyStars' className="d-flex align-items-center my-2 mx-3">
-                          <h5>Difficulty:</h5>
-                          <div id='allStars' className="d-flex ms-2 mb-1">
-                            {Array(recipe.difficulty || 0).fill().map((_, index) => (
-                              <span key={index} className="star">&#9733;</span>
-                            ))}
-                          </div>
+                        <div className="d-flex align-items-center my-2 mx-3">
+                          <h5>Difficulty: {Array(recipe.difficulty || 0).fill().map((_, index) => (
+                              <span key={index} className="fs-4 ms-1 text-center"><StarFill></StarFill></span>
+                            ))}</h5>
                         </div>
-                        <div id='timeCooking' className="d-flex align-items-center my-2 mx-3">
+                        <div className="d-flex align-items-center my-2 mx-3">
                           <h5>Time:</h5>
-                          <p id="time" className="fs-7 fw-bold mt-2">{recipe.cooking_time} min</p>
+                          <p className="fs-5 fw-bold mt-2 ms-2 text-muted">{recipe.cooking_time} min</p>
                         </div>
-                        <div id='energy' className="d-flex align-items-center my-2 mx-3">
-                          <h5>Energy: </h5>
-                          <p className="fs-7 fw-bold mt-2">{recipe.energy ?? 'No info of'} kcal</p>
+                        <div className="d-flex align-items-center my-2 mx-3">
+                          <h5>Energy:</h5>
+                          <p className="fs-5 fw-bold mt-2 ms-2 text-muted">{recipe.energy ?? 'No info of'} kcal</p>
                         </div>
                       </div>
                     </Col>
                   </Col>
-                  <Col xs={12} md={6} lg={6}>
+                  <Col xs={12} md={6} lg={6} className="p-4">
                     <Row>
                       <Col xs={12}>
-                        <div id='ingredient-box' className="mb-3 mt-3 py-2 bg-light box-shadow">
+                        <div className="mb-3 py-2 bg-light box-shadow">
                           <h3>Ingredientes</h3>
-                          <ul id='text-in-left'>
+                          <ul className='text-start'>
                             {recipe.ingredients && recipe.ingredients.map((ingredient, index) => (
-                              <li id='orderList' key={index}>{ingredient.name} - {ingredient.quantity} {ingredient.unit}</li>
+                              <li className='mb-2 fs-6 fw-bold text-muted' key={index}>{ingredient.name} - {ingredient.quantity} {ingredient.unit}</li>
                             ))}
                           </ul>
                         </div>
                       </Col>
                       <Col xs={12}>
-                        <div id='step-box' className="mb-3 bg-danger py-2 text-white box-shadow">
+                        <div className="mb-3 bg-danger py-2 text-white box-shadow">
                           <h3>Pasos</h3>
-                          <ol id='text-in-left'>
+                          <ol className="text-start">
                             {recipe.instructions && recipe.instructions.map((instruction, index) => (
-                              <li id='orderList' className='text-white' key={index}>
-                                Step {instruction.step_number + 1}: {instruction.body}
+                              <li className='mb-2 fs-6 fw-bold text-white' key={index}>
+                                {instruction.body}
                               </li>
                             ))}
                           </ol>
