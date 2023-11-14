8abd42851ff52bbec3353fd4e76d05c7ad556d25
Ivan Mansilla Flores <72189801+ivanmansilla@users.noreply.github.com>
2023-11-14 13:41:49 +0100

    feat: Comment list functional

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,21 +1,30 @@
-import React from "react";
-import { Container, Row, Col} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 
 function Comments(){
-
+    const [comments, setComments] = useState(null);
     return (
     <Container className="" style={{ backgroundColor: '#ffb79fe0'}}>
         <Row className="mt-5">
-            <Col sm={12}>
+            <Col sm={12} className="p-3 box-shadow">
             <h3 className="text-align-center">Comentarios</h3>
             </Col>
-            <Col sm={8}>
+            <Col sm={8} className="mt-3 mx-auto">
                 <Container>
-                    <span>Comentarios no disponibles</span>
+                <ul className="list-unstyled">
+                    {comments ? (
+                    comments.map((comment, index) => (
+                    <li key={index} className="mb-3 p-2 fs-6 bg-light">
+                        {comment.user}: {comment.text}
+                    </li>
+                    ))) :
+                    <span>Comentarios no disponibles</span>}
+                </ul>
                 </Container>
             </Col>
         </Row>
+
     </Container>);
 }
 
