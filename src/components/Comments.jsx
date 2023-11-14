40d29a3c63b56ddc45e7e89cec9f183166079b70
Ivan Mansilla Flores <72189801+ivanmansilla@users.noreply.github.com>
2023-11-14 13:41:01 +0100

    feat: Comment component in progress, styles and responsive

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { Container} from "react-bootstrap";
+import { Container, Row, Col} from "react-bootstrap";
 
 
 function Comments(){
 
     return (
     <Container className="" style={{ backgroundColor: '#ffb79fe0'}}>
+        <Row className="mt-5">
+            <Col sm={12}>
+            <h3 className="text-align-center">Comentarios</h3>
+            </Col>
+            <Col sm={8}>
+                <Container>
+                    <span>Comentarios no disponibles</span>
+                </Container>
+            </Col>
+        </Row>
     </Container>);
 }
 
