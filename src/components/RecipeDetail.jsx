d55d08f71bc869c949dba7724bca98fc6800d609
Ivan Mansilla Flores <72189801+ivanmansilla@users.noreply.github.com>
2023-11-17 16:33:49 +0100

    fix: Aligned with develop and Offcanvas to the right

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -51,88 +51,80 @@ function RecipeDetail() {
           recipeImage={recipe.image ?? gyozas}
           recipeName={recipe.name}
         />
-        <Row className="bg-danger text-white">
-          <Col sm={1} className="py-2"> 
-            <Image src={logo} alt="KASULÀ" fluid />
-          </Col>
-          <Col sm={11}></Col>
-        </Row>
         <Container>
           <Row>
-            <Col sm={2}></Col>
-            <Col sm={8}>
-              <CSSTransition in={true} timeout={500} classNames="slideUp" appear>
-                <Container className="mt-5 text-center box-rounded shadow" style={{ backgroundColor: '#ffb79fe0'}}>
-                  <Row>
-                    <Col xs={12} md={6} lg={6} className="p-4">
+            <Col sm={12}>
+            <CSSTransition in={true} timeout={500} classNames="slideUp" appear>
+              <Container className="mt-5 text-center box-rounded shadow" style={{ backgroundColor: '#ffb79fe0'}}>
+                <Row>
+                  <Col xs={12} md={6} lg={6} className="p-4">
+                    <Col xs={12}>
+                      <Image
+                        src={recipe.image ?? gyozas}
+                        alt={recipe.name}
+                        className="img-fluid shadow mb-3"
+                        onClick={handleOpenModal}
+                        style={{ cursor: "pointer" }}
+                        fluid
+                      />
+                      <h2 style={{ marginBottom: '1rem' }}>{recipe.name}</h2>
+                    </Col>
+                    <Col md={12}>
+                      <div className="mt-5 pb-3 pt-2 bg-light box-shadow">
+                        <h4>Más información</h4>
+                        <div className="d-flex align-items-center my-2 mx-3">
+                          <h5><Image src={chefIcon} style={{height:'24px', width: '24px'}} fluid/> {Array(recipe.difficulty || 0).fill().map((_, index) => (
+                              <span key={index} className="fs-5 ms-1 text-center"><StarFill style={{color: 'gold'}}></StarFill></span>
+                            ))}</h5>
+                        </div>
+                        <div className="d-flex align-items-center my-2 mx-3">
+                          <h5><Stopwatch/></h5>
+                          <span className="fs-6 fw-bold ms-2 text-muted">{recipe.cooking_time} min</span>
+                        </div>
+                        <div className="d-flex align-items-center mt-2 mx-3">
+                          <h5><Lightning/></h5>
+                          <span className="fs-6 fw-bold ms-2 text-muted">{recipe.energy ?? 'No info of'} kcal</span>
+                        </div>
+                        <Button className="mt-3" onClick={handleToggleComments}>
+                          Toggle Comments
+                        </Button>
+                      </div>
+                    </Col>
+                  </Col>
+                  <Col xs={12} md={6} lg={6} className="p-4">
+                    <Row>
                       <Col xs={12}>
-                        <Image
-                          src={recipe.image ?? gyozas}
-                          alt={recipe.name}
-                          className="img-fluid shadow mb-3"
-                          onClick={handleOpenModal}
-                          style={{ cursor: "pointer" }}
-                          fluid
-                        />
-                        <h2 style={{ marginBottom: '1rem' }}>{recipe.name}</h2>
-                      </Col>
-                      <Col md={12}>
-                        <div className="mt-5 pb-3 pt-2 bg-light box-shadow">
-                          <h4>More information</h4>
-                          <div className="d-flex align-items-center my-2 mx-3">
-                            <h5><Image src={chefIcon} style={{height:'24px', width: '24px'}} fluid/> {Array(recipe.difficulty || 0).fill().map((_, index) => (
-                                <span key={index} className="fs-5 ms-1 text-center"><StarFill style={{color: 'gold'}}></StarFill></span>
-                              ))}</h5>
-                          </div>
-                          <div className="d-flex align-items-center my-2 mx-3">
-                            <h5><Stopwatch/></h5>
-                            <span className="fs-6 fw-bold ms-2 text-muted">{recipe.cooking_time} min</span>
-                          </div>
-                          <div className="d-flex align-items-center mt-2 mx-3">
-                            <h5><Lightning/></h5>
-                            <span className="fs-6 fw-bold ms-2 text-muted">{recipe.energy ?? 'No info of'} kcal</span>
-                          </div>
-                          <Button className="mt-3" onClick={handleToggleComments}>
-                            Toggle Comments
-                          </Button>
+                        <div className="mb-3 py-2 bg-light box-shadow">
+                          <h3>Ingredientes</h3>
+                          <ul className='text-start'>
+                            {recipe.ingredients && recipe.ingredients.map((ingredient, index) => (
+                              <li className='mb-2 fs-6 fw-bold text-muted' key={index}>{ingredient.name} - {ingredient.quantity} {ingredient.unit}</li>
+                            ))}
+                          </ul>
                         </div>
                       </Col>
-                    </Col>
-                    <Col xs={12} md={6} lg={6} className="p-4">
-                      <Row>
-                        <Col xs={12}>
-                          <div className="mb-3 p-2 bg-light box-shadow">
-                            <h3>Ingredients</h3>
-                            <ul className='text-start'>
-                              {recipe.ingredients && recipe.ingredients.map((ingredient, index) => (
-                                <li className='mb-2 fs-6 fw-bold text-muted' key={index}>{ingredient.name} - {ingredient.quantity} {ingredient.unit}</li>
-                              ))}
-                            </ul>
-                          </div>
-                        </Col>
-                        <Col xs={12}>
-                          <div className="mb-3 bg-danger p-2 text-white box-shadow">
-                            <h3>Steps</h3>
-                            <ol className="text-start">
-                              {recipe.instructions && recipe.instructions.map((instruction, index) => (
-                                <li className='mb-2 fs-6 fw-bold text-white' key={index}>
-                                  {instruction.body}
-                                </li>
-                              ))}
-                            </ol>
-                          </div>
-                        </Col>
-                      </Row>
-                    </Col>
-                  </Row>
-                </Container>
-              </CSSTransition>
+                      <Col xs={12}>
+                        <div className="mb-3 bg-danger py-2 text-white box-shadow">
+                          <h3>Pasos</h3>
+                          <ol className="text-start">
+                            {recipe.instructions && recipe.instructions.map((instruction, index) => (
+                              <li className='mb-2 fs-6 fw-bold text-white' key={index}>
+                                {instruction.body}
+                              </li>
+                            ))}
+                          </ol>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Container>
+            </CSSTransition>
             </Col>
-            <Col sm={2}></Col>
           </Row>
-      </Container>
+        </Container>
       {/* Offcanvas para mostrar los comentarios */}
-      <Offcanvas show={showComments} onHide={() => setShowComments(false)}>
+      <Offcanvas show={showComments} onHide={() => setShowComments(false)} placement="end">
         <Offcanvas.Header closeButton style={{ backgroundColor: '#ffb79fe0' }}>
           {/* <div> */}
           <Offcanvas.Title className="fs-2 mt-3">Reviews</Offcanvas.Title>
