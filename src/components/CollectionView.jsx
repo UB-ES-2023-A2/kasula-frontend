b6770b97b97918ce34756986d133f287579b2057
Antonio Tigri <entertainmentatv@gmail.com>
2023-12-03 00:54:49 +0100

    Loading Spinner to recipes
    Encapsulated Reipes List and recipes card to their own component!

--- src/components/CollectionView.jsx
+++ src/components/CollectionView.jsx
@@ -1,32 +1,32 @@
 //React
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { CSSTransition } from "react-transition-group";
 import { useAuth } from "./AuthContext";
 
 //Bootstrap
 import { Link } from "react-router-dom";
-import { Container, Row, Col, Card, Image, ListGroup } from "react-bootstrap";
-import { StarFill, ArrowLeft, X } from "react-bootstrap-icons";
+import { Container, Spinner } from "react-bootstrap";
+import { ArrowLeft } from "react-bootstrap-icons";
+
+//Components
+import RecipeList from "./RecipeList";
 
 //CSS
-import "../css/UserFeed.css";
 import "../css/Transitions.css";
-
-//Assets
-import chefIcon from "../assets/icons/chef.png";
-import gyoza from "../assets/gyozas.jpg";
 
 function CollectionView() {
   const { token, isLogged } = useAuth();
   const { id, name } = useParams();
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const navigate = useNavigate();
 
   useEffect(() => {
+    setLoading(true);
     if (!isLogged()) {
       navigate("/login");
+      setLoading(false);
     } else {
       getRecipes();
     }
@@ -38,8 +38,12 @@ function CollectionView() {
       .then((data) => {
         console.log(data);
         setRecipes(data);
+        setLoading(false);
       })
-      .catch((error) => console.error("Error al obtener recetas:", error));
+      .catch((error) => { 
+        console.error("Error al obtener recetas:", error);
+        setLoading(false); 
+      });
   };
 
   return (
@@ -50,84 +54,13 @@ function CollectionView() {
           <ArrowLeft></ArrowLeft>
         </span>
       </Link>
-      <Row className="mt-3">
-        {recipes && recipes.length > 0 ? (
-          recipes.map((recipe) => (
-            <Col sm={12} md={6} xl={4}>
-              <CSSTransition
-                in={true}
-                timeout={500}
-                classNames="slideUp"
-                appear
-              >
-                <div className="position-relative transition-03s" >
-                  <Link
-                    key={recipe._id}
-                    to={`/RecipeDetail/${recipe._id}`}
-                    className="text-decoration-none"
-                  >
-                    <Card className="mt-5 shadow" id="recipes-list">
-                      <Card.Img
-                        className="object-fit-cover"
-                        variant="top"
-                        src={recipe.main_image ?? gyoza}
-                        height={300}
-                      />
-                      <Card.Body>
-                        <Card.Title className="overflow-hidden text-nowrap">
-                          {recipe.name}
-                        </Card.Title>
-                        <h5>
-                          <Image
-                            src={chefIcon}
-                            style={{ height: "24px", width: "24px" }}
-                            fluid
-                          />{" "}
-                          {Array(recipe.difficulty || 0)
-                            .fill()
-                            .map((_, index) => (
-                              <span key={index} className="fs-5 ms-1 text-center">
-                                <StarFill style={{ color: "gold" }}></StarFill>
-                              </span>
-                            ))}
-                        </h5>
-                        <div className="d-flex">
-                          <StarFill className="mx-1 mt-1" style={{color: 'red'}}></StarFill>
-                          <span>{recipe?.average_rating?.toFixed(1) || 0}</span>
-                        </div>
-                      </Card.Body>
-                      <Card.Footer>By {recipe?.username}</Card.Footer>
-                    </Card>
-                  </Link>
-                  <span className="fs-3 colorless-span-button position-absolute top-0 end-0 mx-2" role="button" onClick={
-                    () => {
-                      fetch(process.env.REACT_APP_API_URL + `/collection/${id}/remove_recipe/${recipe._id}`, {
-                        method: 'PUT',
-                        headers: {
-                          Authorization: `Bearer ${token}`,
-                        },
-                      })
-                        .then((response) => response.json())
-                        .then((data) => {
-                          console.log(data);
-                          getRecipes();
-                        })
-                        .catch((error) => console.error("Error al obtener recetas:", error));
-                    }
-                  }>
-                    <X className="rounded-circle highlighter" />
-                  </span>
-                </div>
-              </CSSTransition>
-            </Col>
-          ))
-        ) : (
-          <div className="alert alert-warning mt-4" role="alert">
-            This collection has no recipes. Search for a recipe in your feed,
-            profile or another user and add it from there.
-          </div>
-        )}
-      </Row>
+      {loading ? (
+        <Container className="d-flex justify-content-center align-items-center">
+          <Spinner animation="border" variant="secondary" />
+        </Container>
+      ) : (
+        <RecipeList recipes={recipes} canDelete={true} id={id} token={token} onDeleteRecipe={getRecipes} />
+      )}
     </Container>
   );
 }
